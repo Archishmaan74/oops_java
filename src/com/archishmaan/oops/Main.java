d2c960389562dc1b123150549398f1821c295af0
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-06-15 21:33:27 +0530

    Update Main.java

--- src/com/archishmaan/oops/Main.java
+++ src/com/archishmaan/oops/Main.java
@@ -3,7 +3,7 @@ package com.archishmaan.oops;
 public class Main {
     public static void main(String[] args) {
         Pen pen1 = new Pen(); // Pen() here is a default constructor which does not require to be defined in class
-        Student s1 = new Student("Archishmaan", 24); // Parameterised constructor
+        Student s1 = new Student("Archishmaan", 24); // Parameterised constructor, values assigned inside class
         String color1 = (pen1.color = "Blue");
         String type1 = (pen1.type = "Gel");
         String s1Name = s1.name;
