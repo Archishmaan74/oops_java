b0dc5de12036a4d62d0c570b57f5f81aeb452ecd
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-06-17 15:34:39 +0530

    access modifiers usecases

--- src/com/archishmaan/bank/Account.java
+++ src/com/archishmaan/bank/Account.java
@@ -2,4 +2,15 @@ package com.archishmaan.bank;
 
 public class Account{
     public String name;
+    protected String email; // Only accessible in the same package or in subclass of Account in different package
+    private String password; // only accessible to the class
+    // For accessing private members of class we use getters and setter
+    // getter
+    public String getPassword(){
+        return this.password;
+    }
+    // setter
+    public void setPassword(String pass){
+        this.password = pass;
+    }
 }
--- src/com/archishmaan/bank/Bank.java
+++ src/com/archishmaan/bank/Bank.java
@@ -1,4 +1,11 @@
 package com.archishmaan.bank;
 
 public class Bank {
+    public static void main(String[] args){
+        Account account1 = new Account();
+        account1.email = "uarchishmaan@yahoo.com";
+        account1.setPassword("123456");
+        System.out.println( account1.email + " is protected and being used in same package");
+        System.out.println("Accessed password: " + account1.getPassword());
+    }
 }
