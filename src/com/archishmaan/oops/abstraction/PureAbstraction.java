25a7e093a25ca0c05717bb4d07d7df58542d2dad
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-06-17 18:13:28 +0530

    interfaces

--- src/com/archishmaan/oops/abstraction/PureAbstraction.java
+++ src/com/archishmaan/oops/abstraction/PureAbstraction.java
@@ -1,4 +1,18 @@
 package com.archishmaan.oops.abstraction;
 
+interface Animals{
+    int IQ = 2;
+    void walk();
+}
+
+class Horses implements Animals{
+    public void walk(){
+        System.out.println("Walks on 4 legs and has an IQ of " + IQ);
+    }
+}
 public class PureAbstraction {
+    public static void main(String[] args){
+        Horses poloRunner = new Horses();
+        poloRunner.walk();
+    }
 }
